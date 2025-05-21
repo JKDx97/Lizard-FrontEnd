3bdeb534f25772ed8c3de2b21f32369689f09932
JKDx97 <hoterosebastian2004@gmail.com>
2025-05-21 01:04:58 -0500

    Actualiacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private socket!: Socket;
-  private isLoggedIn = false; 
-  private userDataSubject = new BehaviorSubject<any>(null); 
-  userData$ = this.userDataSubject.asObservable(); 
+  private isLoggedIn = false;
+  private userDataSubject = new BehaviorSubject<any>(null);
+  userData$ = this.userDataSubject.asObservable();
 
   constructor() {
-    this.socket = io(`${environment.api}`);
+    this.socket = io(environment.api, {
+      transports: ['websocket', 'polling'],
+      withCredentials: true,
+    });
     this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-    const storedUserData = JSON.parse(localStorage.getItem('userData') || 'null');
+    const storedUserData = JSON.parse(
+      localStorage.getItem('userData') || 'null'
+    );
     if (storedUserData) {
-      this.userDataSubject.next(storedUserData); 
+      this.userDataSubject.next(storedUserData);
     }
   }
 
@@ -26,18 +31,18 @@ export class AuthService {
       if (!this.socket.connected) {
         this.socket.connect();
       }
-  
+
       this.socket.emit('login', { username, password });
-  
+
       this.socket.on('login_success', (data) => {
         this.isLoggedIn = true;
         localStorage.setItem('isLoggedIn', 'true');
-        localStorage.setItem('userData', JSON.stringify(data.user)); 
-        this.userDataSubject.next(data.user); 
+        localStorage.setItem('userData', JSON.stringify(data.user));
+        this.userDataSubject.next(data.user);
         observer.next(data);
         observer.complete();
       });
-  
+
       this.socket.on('login_error', (error) => {
         observer.error(error);
       });
@@ -48,18 +53,18 @@ export class AuthService {
     return this.userDataSubject.getValue();
   }
 
-  getUserDataId(){
-    return this.userDataSubject.value?._id
+  getUserDataId() {
+    return this.userDataSubject.value?._id;
   }
-  getUserUsername(){
-    return this.userDataSubject.value?._username
+  getUserUsername() {
+    return this.userDataSubject.value?._username;
   }
 
   logout(): void {
     this.isLoggedIn = false;
     localStorage.removeItem('isLoggedIn');
     localStorage.removeItem('userData');
-    this.userDataSubject.next(null); 
+    this.userDataSubject.next(null);
 
     this.socket.emit('logout');
     if (this.socket) {
